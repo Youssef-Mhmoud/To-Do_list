c4c7c9aea72ef9bbe357b4ebd6012e574f7a665d
Youssef-Mahmoud-web <joshuman526@gmail.com>
2022-05-29 05:39:38 +0200

    Add Finish The To-Do Task

--- main.js
+++ main.js
@@ -2,34 +2,32 @@
 let h1 = document.querySelector("h1");
 
 let theDate = new Date();
-  h1.innerHTML = theDate.toDateString();
+h1.innerHTML = theDate.toDateString();
 
 // Get Elements
 // let btnAdd = document.getElementById("add");
 let input = document.getElementById("input");
 let taskCont = document.querySelector(".cont-task");
-let form = document.querySelector('form')
+let form = document.querySelector("form");
 
 // Add A Task
 form.addEventListener("submit", (eo) => {
-  // Stop Reload The Page 
-  eo.preventDefault()
+  // Stop Reload The Page
+  eo.preventDefault();
 
   taskCont.innerHTML += `
   <div class="task">
-    <p>${input.value}</p>
+    <p class="text-line">${input.value}</p>
     <div>
       <i class="fa-solid fa-xmark"></i>
-      <input type="checkbox" class="check">
     </div>
-  </div>`
+  </div>`;
 });
 
 // Remove A Task
-taskCont.addEventListener('click', (eo) => {
+taskCont.addEventListener("click", (eo) => {
   if (eo.target.className == "fa-solid fa-xmark") {
-    eo.target.parentElement.parentElement.remove()
-  } else {
-
+    eo.target.parentElement.parentElement.remove();
   }
-})
+  eo.target.getElementsByClassName("text-line")[0].classList.toggle("check");
+});
