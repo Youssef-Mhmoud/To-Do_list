7674d9484ab3ac6586a9bc8016590fb252564e1e
Youssef-Mahmoud-web <joshuman526@gmail.com>
2022-05-29 02:49:49 +0200

    Modified Three Files

---
+++ main.js
@@ -0,0 +1,35 @@
+// Add A Date To <H1>
+let h1 = document.querySelector("h1");
+
+let theDate = new Date();
+  h1.innerHTML = theDate.toDateString();
+
+// Get Elements
+// let btnAdd = document.getElementById("add");
+let input = document.getElementById("input");
+let taskCont = document.querySelector(".cont-task");
+let form = document.querySelector('form')
+
+// Add A Task
+form.addEventListener("submit", (eo) => {
+  // Stop Reload The Page 
+  eo.preventDefault()
+
+  taskCont.innerHTML += `
+  <div class="task">
+    <p>${input.value}</p>
+    <div>
+      <i class="fa-solid fa-xmark"></i>
+      <input type="checkbox" class="check">
+    </div>
+  </div>`
+});
+
+// Remove A Task
+taskCont.addEventListener('click', (eo) => {
+  if (eo.target.className == "fa-solid fa-xmark") {
+    eo.target.parentElement.parentElement.remove()
+  } else {
+
+  }
+})
